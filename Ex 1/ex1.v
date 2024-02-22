44722ff932db7f28681bfb26ed8144def8a7d632
RK Mañago <rbmanago1@up.edu.ph>
2024-02-22 13:40:05 +0800

    feat(not done): add ex1 coq file

---
+++ Ex 1/ex1.v
@@ -0,0 +1,50 @@
+Inductive bool : Type := 
+    |true
+    |false.
+
+Definition notb (b: bool) : bool :=
+    if b then false
+    else true.
+
+Definition orb (b1: bool) (b2: bool) : bool :=
+    match b1 with 
+    |true => true
+    |false => b2
+    end.
+
+Definition andb (b1: bool) (b2: bool) : bool :=
+    match b1 with
+    |false => false
+    |true => b2
+    end.
+
+Definition nandb (b1: bool) (b2: bool) : bool :=
+    notb(andb b1 b2).
+
+Example test_nandb: (nandb true true) = false.
+Proof. simpl. reflexivity. Qed.
+
+Definition andb3 (b1: bool) (b2: bool) (b3: bool) : bool :=
+    if notb(b1) then false
+    else if notb(b2) then false
+    else if notb(b3) then false
+    else true.
+
+Example test_andb3: (andb3 true true false) = false.
+Proof. simpl. reflexivity. Qed.
+
+Fixpoint minus (n m : nat) : nat := 
+    match n, m with 
+    | O , _ => O (*the _ character is a catch-all regex*)
+    | S _ , O => n
+    | S n' , S m' => minus n' m'
+    end.
+
+Fixpoint mult (n : nat) (m : nat) : nat :=
+    match n with
+    |O => O
+    |S n' => plus m (mult n' m)
+    end. 
+
+Fixpoint factorial (n : nat) : nat
+    
