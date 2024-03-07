69fe906eb368936171844b9d3d6b6a5fda6be3cd
RK Mañago <rbmanago1@up.edu.ph>
2024-03-07 21:52:24 +0800

    feat: finish items 1-3 of exercise 2

---
+++ Ex 2/ex2.v
@@ -0,0 +1,55 @@
+Definition orb (b1: bool) (b2: bool) : bool :=
+    match b1 with 
+    |true => true
+    |false => b2
+    end.
+
+Definition andb (b1: bool) (b2: bool) : bool :=
+    match b1 with
+    |false => false
+    |true => b2
+    end. 
+
+(* Answers *)
+
+Theorem plus_id_exercise : forall n m o : nat, n = m -> m = o -> n + m = m + o.
+Proof.
+    intros.
+    rewrite -> H.
+    rewrite -> H0.
+    reflexivity.
+Qed.
+
+Search ( mult ).
+
+Theorem mult_n_1 : forall p : nat, p * 1 = p.
+Proof.
+    intros.
+    destruct p.
+    - reflexivity.
+    - rewrite <- mult_n_Sm. simpl. 
+    rewrite <- mult_n_O. reflexivity.
+Qed.
+
+Theorem identity_fn_applied_twice : forall (f : bool -> bool), (forall (x : bool), f x = x) -> forall (b : bool), f (f b) = b.
+Proof.
+    intros.
+    rewrite -> H.
+    rewrite -> H.
+    reflexivity.
+Qed.
+
+Theorem andb_eq_orb : forall (b c : bool), (andb b c = orb b c) -> b = c.
+Proof.
+    destruct b.
+    intros.
+    destruct c.
+    reflexivity.
+    inversion H.
+
+Qed.
+
+Theorem mul_0_r : ∀ n:nat, n × 0 = 0.
+Theorem add_assoc : ∀ n m p : nat, n + (m + p) = (n + m) + p.
+Theorem eqb_refl : ∀ n : nat, (n =? n) = true.
+Theorem even_S : ∀ n : nat, even (S n) = negb (even n).
