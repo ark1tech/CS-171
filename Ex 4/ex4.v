a15c05638f3afdca53035a980db93a3c47769ce3
Jose <joseernestotmanan@gmail.com>
2024-05-05 23:10:45 +0800

    feat: start ex4, finish item 1

---
+++ Ex 4/ex4.v
@@ -0,0 +1,93 @@
+(* --------------- PREFACE --------------- *)
+
+Inductive list (X:Type) : Type :=
+    | nil
+    | cons (x : X) (l : list X).
+
+Fixpoint repeat (X : Type) (x : X) (count : nat) : list X :=
+    match count with
+    | 0 => nil X
+    | S count'
+    => cons X x (repeat X x count')
+    end.
+
+(* Notation "x :: l" := (cons x l)
+    (at level 60, right associativity). *)
+Notation "[ ]" := nil.
+Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..).
+
+Arguments nil {X}.
+Arguments cons {X}.
+Arguments repeat {X}.
+
+Fixpoint app {X : Type} (l1 l2 : list X) : list X :=
+    match l1 with
+    | nil => l2
+    | cons h t => cons h (app t l2)
+    end.
+
+Fixpoint rev {X:Type} (l:list X) : list X :=
+    match l with
+    | nil => nil
+    | cons h t => app (rev t) (cons h nil)
+    end.
+
+Fixpoint length {X : Type} (l : list X) : nat :=
+    match l with
+    | nil => 0
+    | cons _ l' => S (length l')
+    end.
+
+Notation "x ++ y" := (app x y)
+    (right associativity, at level 60).
+
+Theorem app_assoc : forall A (l m n:list A), l ++ m ++ n = (l ++ m) ++ n.
+Proof.
+    intros A l m n.
+    induction l as [ | n' l' IHl' ].
+    - simpl. reflexivity.
+    - simpl. rewrite IHl'. reflexivity.
+Qed.
+
+(* Fixpoint filter {X:Type} (test: X -> bool) (l:list X) : list X :=
+    match l with
+    | [] => []
+    | h :: t =>
+        if test h then h :: (filter test t)
+        else filter test t
+    end. *)
+
+(* Definition partition {X : Type} (test : X -> bool)
+    (l : list X) : list X * list X :=
+    ((filter test l),(filter (fun n => negb (test n)) l)). *)
+
+(* Fixpoint map {X Y : Type} (f : X->Y) (l : list X) : list Y :=
+    match l with
+    | [] => []
+    | h :: t => (f h) :: (map f t)
+    end. *)
+
+(* --------------- ANSWERS --------------- *)
+
+Theorem app_nil_r : forall (X : Type), forall (l : list X),
+    l ++ [ ] = l.
+Proof.
+    intros.
+    induction l.
+    - simpl. reflexivity.
+    - simpl. rewrite IHl. reflexivity.
+Qed.
+
+Theorem rev_involutive : forall (X : Type), forall (l : list X),
+    rev (rev l) = l.
+Proof.
+    intros.
+    induction l.
+    - simpl. reflexivity.
+    - simpl. 
+Qed.
+
+(* Example test_filter_even_gt7_1 : filter_even_gt7 [1;2;6;9;10;3;12;8] = [10;12;8]. *)
+
+(* Theorem map_rev : forall (X Y : Type) (f : X -> Y) (l : list X),
+    map f (rev l) = rev (map f l). *)
