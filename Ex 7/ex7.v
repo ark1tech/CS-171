993c09edf2423e444b8b7f985ec209b8059e1c89
RK Mañago <rbmanago1@up.edu.ph>
2024-05-20 15:28:38 +0800

    feat: add progress ex7

--- Ex 7/ex7.v
+++ Ex 7/ex7.v
@@ -4,6 +4,14 @@ Inductive ev : nat -> Prop :=
     | ev_0 : ev 0
     | ev_SS (n : nat) (H : ev n) : ev (S (S n)).
 
+Inductive total_relation : nat -> nat -> Prop :=
+    | tr : forall n m, total_relation n m.
+
+Theorem evSS_ev : forall n, ev (S (S n)) -> ev n.
+Proof.
+    intros n H. inversion H. apply H1.
+Qed.
+
 (* --------------- ANSWERS --------------- *)
 
 Theorem SSSSev__even : forall n,
@@ -12,24 +20,34 @@ Proof.
     intros.
     induction n.
     - apply ev_0.
-    - 
+Admitted.
 
-(* Theorem ev5_nonsense :
+Theorem ev5_nonsense :
     ev 5 -> 2 + 2 = 9.
-Admitted. *)
+Admitted. 
 
-(* Theorem ev_sum : forall n m,
+Theorem ev_sum : forall n m,
     ev n -> ev m -> ev (n + m).
-Admitted. *)
+Admitted.
 
-(* Theorem ev_ev__ev : forall n m,
+Theorem ev_ev__ev : forall n m,
     ev (n+m) -> ev n -> ev m.
-Admitted. *)
+Proof.
+    intros.
+    induction H0.
+    - simpl in H. apply H.
+    - simpl in H. apply IHev. apply evSS_ev in H. apply H.
+Qed.
 
-(* Theorem ev_plus_plus : forall n m p,
+Theorem ev_plus_plus : forall n m p,
     ev (n+m) -> ev (n+p) -> ev (m+p).
-Admitted. *)
+Proof.
+    intros.
+    inversion H 
+Admitted.
+    
+Admitted.
 
-(* Theorem total_relation_is_total : forall n m,
+Theorem total_relation_is_total : forall n m,
     total_relation n m.
-Admitted. *)
+Admitted.
