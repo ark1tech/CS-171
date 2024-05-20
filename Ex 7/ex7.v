7a050c77aaa285233cff238b5870d068cf9f43c7
Jose <joseernestotmanan@gmail.com>
2024-05-20 14:55:52 +0800

    feat: started ex7

---
+++ Ex 7/ex7.v
@@ -0,0 +1,35 @@
+(* --------------- PREFACE --------------- *)
+
+Inductive ev : nat -> Prop :=
+    | ev_0 : ev 0
+    | ev_SS (n : nat) (H : ev n) : ev (S (S n)).
+
+(* --------------- ANSWERS --------------- *)
+
+Theorem SSSSev__even : forall n,
+    ev (S (S (S (S n)))) -> ev n.
+Proof.
+    intros.
+    induction n.
+    - apply ev_0.
+    - apply ev_SS in H.
+
+(* Theorem ev5_nonsense :
+    ev 5 -> 2 + 2 = 9.
+Admitted. *)
+
+(* Theorem ev_sum : forall n m,
+    ev n -> ev m -> ev (n + m).
+Admitted. *)
+
+(* Theorem ev_ev__ev : forall n m,
+    ev (n+m) -> ev n -> ev m.
+Admitted. *)
+
+(* Theorem ev_plus_plus : forall n m p,
+    ev (n+m) -> ev (n+p) -> ev (m+p).
+Admitted. *)
+
+(* Theorem total_relation_is_total : forall n m,
+    total_relation n m.
+Admitted. *)
