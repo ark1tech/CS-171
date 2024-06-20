b575df4d538987109cbfb516b931bee5dd7f078a
Jose <joseernestotmanan@gmail.com>
2024-06-20 20:13:06 +0800

    fix: returning use case WAHAHAHHA

--- Project/project.v
+++ Project/project.v
@@ -6,6 +6,16 @@ From Coq Require Import Arith.EqNat. Import Nat.
 From Coq Require Import Lia.
 From Coq Require Import Lists.List. Import ListNotations.
 
+(*------------------USE CASE FOR PACEMAKER ------------------
+    A sick client wants you to make an app that maintains a normal heart rate.
+    The normal heart rate is 60 to 100 BPM.
+    App constraints:
+    1) The heart's BPM should stay in between a certain threshold, which we define as 60 and 100 (normal heart rate).
+    2) Any time the heart rate goes below this limit, a weak electrical signal is produced to create an artificial heart beat to compensate.
+    3) If the heart rate goes above this limit, a strong electrical signal is produced to restart the heart's rhythm.
+    4) If there is no natural heartbeat detected within the last 60 seconds, a strong electrical signal is produced to restart the heart's rhythm.
+*)
+
 (*------------------HELPERS------------------*)
 Fixpoint lebnat (n m : nat) : bool :=
     match n with
